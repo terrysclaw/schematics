7fe22f9200f7c8f7ce83d9ae29c55ffe4b452569
Terry Law <tl@ixx.cc>
2022-01-16 20:07:31 +0800

    update index.ts

--- src/crud/index.ts
+++ src/crud/index.ts
@@ -6,16 +6,16 @@ import { strings } from '@angular-devkit/core';
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function crud(_options: Schema): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-      const sourceTemplate = url('./files'); // 取得 template 檔案
-    const sourceParameterizedTemplates = apply(sourceTemplate, [
-      template({
-        ..._options,
-        ...strings,
-      })
-    ]);
+    return (tree: Tree, _context: SchematicContext) => {
+        const sourceTemplate = url('./files'); // 取得 template 檔案
+        const sourceParameterizedTemplates = apply(sourceTemplate, [
+            template({
+                ..._options,
+                ...strings,
+            })
+        ]);
 
-    return mergeWith(sourceParameterizedTemplates)(tree, _context);
+        return mergeWith(sourceParameterizedTemplates)(tree, _context);
 
-  };
+    };
 }
